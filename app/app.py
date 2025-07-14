20fd1b6b4cc0c991c9c672e99b2eda9dd31b5ca1
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2025-07-14 17:27:46 +0200

    initial commit

---
+++ app/app.py
@@ -0,0 +1,5 @@
+# app/main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+# app.include_router(users.router, prefix="/api/users", tags=["Users"])
---
+++ app/db/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
