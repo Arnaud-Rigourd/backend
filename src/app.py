2b7dfb107fbac2e1827e44ebe8cc6818077f659b
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2025-07-29 23:57:12 +0200

    remove useless files

--- src/app.py
+++ src/app.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
 
-from app.api.v1.router import api_router
-
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
+def ping():
     """A simple endpoint to confirm the server is running."""
     return {"status": "ok"}
 
-
-app.include_router(api_router, prefix="/api/v1")
--- src/db/dependencies.py
+++ src/db/dependencies.py
@@ -2,7 +2,7 @@ from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.db.session import async_session
+from src.db.session import async_session
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
